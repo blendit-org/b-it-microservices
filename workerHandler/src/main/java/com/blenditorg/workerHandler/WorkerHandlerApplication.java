f782f69cb2ea48140d2b56703538cb8ea9a4e39c
pritomshad <2021331039@student.sust.edu>
2025-09-09 00:54:57 +0600

    Project Queue Added, Worker handler fetches job from queue

---
+++ workerHandler/src/main/java/com/blenditorg/workerHandler/WorkerHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.blenditorg.workerHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerHandlerApplication.class, args);
+	}
+
+}
